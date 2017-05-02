045006f14420db2920293aa5dc1384127f704453
smeleyka <smeleyka@gmail.com>
2017-05-02 19:08:12 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         String[]words = {"Яблоко","Яблоко","Абрикос","Авокадо","Вишня","Оливки","Манго","Папайя","Персики","Груши","Сливы","Миндаль","Кешью","Вишня","Каштан","Финики","Фундук","Кумкват","Лимон","Каштан","Оливки","Лайм"};
         HashSet<String> wordsHash = new HashSet<String>(Arrays.asList(words));
         System.out.println(wordsHash);
+        System.out.println("Всего "+wordsHash.size()+" слов.");
 ////////////// Задание 2 ////////////////////////////////////
 
             Window w=new Window();
